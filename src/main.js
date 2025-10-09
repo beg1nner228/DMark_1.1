5cb2039ed91fe87f24c6961ef617d8b7540bed0d
beg1nner228 <00mp5@bbzolten.ch>
2025-10-09 14:39:03 +0200

    Update htmls

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import './JS/theme.js';
 import './JS/i18n.js';
 import './css/reset.css';
 import './css/common.css';
@@ -6,11 +7,21 @@ import './css/container.css';
 import './css/history&settings_modals.css';
 import './JS/bookmarks/header.js';
 import './JS/recently-used-slider.js'
-import './JS/theme.js';
 import './JS/history.js';
 import './JS/recent-links.js';
 import './JS/settings.js';
 
 
-
+export function applyAndSaveTheme(theme, saveToStorage = true) {
+    document.body.classList.remove('force-light-theme', 'force-dark-theme'); // Это может вызвать мигание!
+    // ...
+    if (actualTheme === "dark") {
+        body.classList.add("force-dark-theme");
+        if (img) img.src = moon;
+    } else {
+        body.classList.remove("force-dark-theme"); // Здесь body.classList.remove("force-dark-theme");
+        if (img) img.src = sun;
+    }
+    // ...
+}
 
